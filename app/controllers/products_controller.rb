87e269a168eeb2739948057152efdf9ae32f64d2
apoon- <anson.os.poon@gmail.com>
2013-11-16 17:58:48 -0500

    index displaying pictures and basic show page functioning

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,11 +3,13 @@ require 'open-uri'
 class ProductsController < ApplicationController
 
   def index
-    products_json = open('http://lcboapi.com/products').read
-    @products = JSON.parse(products_json)
+    @products = JSON.parse(open('http://lcboapi.com/products').read)
   end
 
   def show
+    product_json = open("http://lcboapi.com/products/#{params[:id]}").read
+    index_json = JSON.parse(product_json)
+    @product = index_json['result']
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def format_numbers(numbers)
+    processed = numbers.to_f/100.00
+    sprintf("%.2f", processed)
+  end
 end
