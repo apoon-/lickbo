b90ee5cbb9b45464a315c3bd1fa3f1316a4dac4f
apoon- <anson.os.poon@gmail.com>
2013-11-16 15:02:41 -0500

    initial commit uninitialized constant error

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,13 @@
+require 'open-uri'
+
+class ProductsController < ApplicationController
+
+  def index
+    products_json = open('http://lcboapi.com/products').read
+    @products = JSON.parse(products_json)
+  end
+
+  def show
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lickbo::Application.config.secret_key_base = '500ff731c9e005e2b5f817eb12c357dc44ddb3ba656b8b1600370b561e9be48adf2e132c6ce981dd0c465c96ff67f0fae0259707ca11906cd7d0c3f8c7d58f2f'
